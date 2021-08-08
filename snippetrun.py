deb5ad06d050971ba6becb1a2417224e87b6bfad
spa <spa.in.spa@gmail.com>
2021-08-08 23:54:07 +0300

    transitional commit

--- snippetrun.py
+++ snippetrun.py
@@ -5,7 +5,20 @@ import time
 
 class SnippetRun:
     def __init__(self):
-        self.usernanme = ''
+        self.username = ''
         self.password = ''
+        self.data_folder_path = ''
         self.snippet = []
         self.devices = []
+
+    def get_credentials(self):
+        self.data_folder_path = input('Full path to data files folder: ')
+        self.username = input('SSH username: ')
+        self.password = getpass.getpass('SSH password: ')
+
+    def load_snippet(self):
+        pass
+
+    def load_devices(self):
+        pass
+
