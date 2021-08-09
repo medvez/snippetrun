534b978587ced324678af73f8aa80e5fca36e23d
Sergey Anikin <spa.in.spa@gmail.com>
2021-08-09 13:34:18 +0300

    V1.0

--- snippetrun.py
+++ snippetrun.py
@@ -1,6 +1,7 @@
 import paramiko
 import getpass
 import time
+import os.path
 
 
 class SnippetRun:
@@ -10,15 +11,51 @@ class SnippetRun:
         self.data_folder_path = ''
         self.snippet = []
         self.devices = []
+        self.device_ip = ''
 
     def get_credentials(self):
         self.data_folder_path = input('Full path to data files folder: ')
         self.username = input('SSH username: ')
-        self.password = getpass.getpass('SSH password: ')
+        self.password = getpass.getpass(prompt='SSH password: ')
 
     def load_snippet(self):
-        pass
+        _snippet_full_path = os.path.join(self.data_folder_path, 'snippet.txt')
+        with open(file=_snippet_full_path, mode='r', encoding='utf8') as file_content:
+            for line in file_content:
+                self.snippet.append(line)
 
     def load_devices(self):
-        pass
+        _devices_full_path = os.path.join(self.data_folder_path, 'devices.txt')
+        with open(file=_devices_full_path, mode='r', encoding='utf8') as file_content:
+            for line in file_content:
+                if line:
+                    self.devices.append(line[:-1])
 
+    def ssh_operation(self, device_ip):
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        print('Connecting to', device_ip)
+        client.connect(hostname=device_ip, username=self.username, password=self.password)
+        print('Connected!')
+        ssh_session = client.invoke_shell()
+        time.sleep(1)
+        for command in self.snippet:
+            ssh_session.send(command)
+            time.sleep(1)
+        time.sleep(4)
+        client.close()
+
+    def configure_devices(self):
+        for device_ip in self.devices:
+            self.ssh_operation(device_ip)
+
+    def run(self):
+        self.get_credentials()
+        self.load_snippet()
+        self.load_devices()
+        self.configure_devices()
+
+
+if __name__ == '__main__':
+    snippetrun = SnippetRun()
+    snippetrun.run()
